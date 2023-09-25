327e4e4e006e1d74f7d9f37a2784684cd94bf80a
Doppelgangers <6666genocide666@mail.ru>
2023-09-25 23:40:30 +0300

    redesigned the file separator~

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 ROOT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
 
 """Для правильной работы PyDub"""
-FFMPEG_PATH = ROOT_PATH.joinpath(r"ffmpeg\bin")
+# FFMPEG_PATH = ROOT_PATH.joinpath(r"ffmpeg\bin")
+FFMPEG_PATH = Path(r"D:\Desktop\ffmpeg-master-latest-win64-gpl-shared\bin")
 os.environ["PATH"] += os.pathsep + FFMPEG_PATH.__str__()
 
